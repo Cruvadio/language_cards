9e0f763e12408355decc1037c145d3f9001a696d
Cruvadio <ialexandrova53@gmail.com>
2020-06-12 03:59:22 +0300

    Refactored CSS code into LESS and made some visual improvements

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 var myImage = document.querySelector('img');
 
-myImage.onclick = function()
-{
+myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if (mySrc == 'images/american-flag-png-circle-6-original.png'){
+    if (mySrc == 'images/american-flag-png-circle-6-original.png') {
         myImage.setAttribute('src', 'images/scale_1200.webp');
     } else {
         myImage.setAttribute('src', 'images/american-flag-png-circle-6-original.png')
@@ -17,8 +16,8 @@ function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
     myHeading.textContent = 'Welcome, ' + myName;
-  }
+}
 
 myButton.onclick = function() {
-  setUserName();
+    setUserName();
 }
